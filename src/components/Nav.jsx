a12c4f41288ac2fff00ffe81ec739b27b00ec404
Charlotte <smit-charlotte@outlook.com>
2025-06-18 15:20:53 +0200

    navbar DONE

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,22 @@ import './nav.css'
 import logo from '../assets/img/ph-logo-white.webp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router'
+
 
 export default function Nav() {
+
+
 
     return(
         <>
         <nav>
-            <img src={logo} alt="" />
+            <Link to="/" className="d-flex align-items-center"><img src={logo} alt=""/></Link>
             <div className="d-flex align-items-center gap-3">
                 <div className="d-flex align-items-center h-100">
                     <button>Se connecter</button>
                 </div>
-                <div className="d-flex h-100 align-items-center">
+                <div className="d-flex h-100 align-items-center" role="button">
                     FR&nbsp;<FontAwesomeIcon icon={faAngleDown} />
                 </div>
             </div>
