ab2fbcfba86436f9a9be43fd9b78a348140871cf
molengeek <saoussan.elk@gmail.com>
2025-06-18 15:13:07 +0200

    map

--- src/components/Contenu.jsx
+++ src/components/Contenu.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router"
+import pizzaData from '../../pizzas.json'
+
+
 export default function Contenu () {
 
     return(
 
         <>
+            {pizzaData.map((pizzaData) => (
+                <div 
+                    key={pizzaData.name} 
+                    className="cardLink"
+                >
+                    <div className="cardOne">
+                        <div className="cardImg">
+                            <img src={pizzaData.image} alt="" />
+                        </div>
+
+                    </div>
+
+                </div>
+            ))}
         
         </>
     )
