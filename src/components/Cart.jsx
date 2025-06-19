d8bb469fd37d021a98cdca49c772bdd07973d253
Charlotte <smit-charlotte@outlook.com>
2025-06-19 11:19:11 +0200

    update css cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,38 +13,47 @@ export default function Cart() {
 
         <>
   
-        <div className="cart">
-
-            <h2>Panier d'achat</h2>
-            <p>Quantité : {cartItems.length}</p>
-            <div className="">
-                {cartItems.map((item, index) => {
-                    return (
-                        <div key={index} className="d-flex flex-column">
-                            <div className="d-flex gap-2">
-                                <p>{item.name} - €{item.price}</p>
-                                <div>
-                                    <button onClick={() => dispatch(deletePizza(item.name))}>X</button>
-                                </div>
-                            </div>
+        <div className="cart d-flex flex-column justify-content-between">
+            {/* Recap panier */}
+            <div className="panier d-flex flex-column justify-content-between border border-3">
+                <div className="h-100 border cart-bis">
+                {/* Titre */}
+                    <h2>Panier d'achat</h2>
+                        {/* Items ajoutés au panier */}
+                        <div className="cart-content">
+                            {cartItems.map((item, index) => {
+                                return (
+                                    <div key={index} className="d-flex flex-column">
+                                        <div className="d-flex gap-2">
+                                            <p>{item.name} - €{item.price}</p>
+                                            <div>
+                                                <button onClick={() => dispatch(deletePizza(item.name))}>X</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    )
-                })}
-                <div>
-                    <h4>Total: € {result}</h4>
                 </div>
+                {/* Total */}
+                <div className="total">
+                        <h4>Total: € {result}</h4>
+                </div>
+
+
             </div>
 
+            {/* Bouton Commander */}
             <div>
-                <button className="w-100 d-flex justify-content-evenly">
+                <button disabled className="w-100 commander d-flex justify-content-evenly">
                     <div>
-                        {cartItems.length}
+                        <p className="p-0 m-0">{cartItems.length}</p>
                     </div>
                     <div>
                         <p className="p-0 m-0">Commander</p>
                     </div>
                     <div>
-                        {result}
+                        <p className="p-0 m-0">€ {result}</p>
                     </div>
 
                 </button>
