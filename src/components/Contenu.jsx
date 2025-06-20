29d11e85a44593f0fa7de4b90e2b56cffab20fbb
Charlotte <smit-charlotte@outlook.com>
2025-06-20 12:14:50 +0200

    update hover cards

--- src/components/Contenu.jsx
+++ src/components/Contenu.jsx
@@ -1,49 +1,56 @@
-import { Link } from "react-router-dom"
-import pizzaData from '../../pizzas.json'
-import './contenu.css'
-import Cart from "./Cart"
-import { useDispatch } from 'react-redux'
-import { addPizza } from '../redux/panierSlice'
+    import { Link } from "react-router-dom"
+    import pizzaData from '../../pizzas.json'
+    import './contenu.css'
+    import Cart from "./Cart"
+    import { useDispatch } from 'react-redux'
+    import { addPizza } from '../redux/panierSlice'
+    import { useState } from "react"
 
 
-export default function Contenu () {
-    // test :
-    const dispatch = useDispatch()
 
-    return(
-        <>
-        <div className="d-flex content">
-            <div className="contenuA">
-                {pizzaData.map((pizzaData) => (
-                    <div 
-                        key={pizzaData.name} 
-                        className="cardLink"
-                    >
-                        {/* <div className="cardOne"> */}
-                        <div className="cardImg">
-                            <img src={pizzaData.image} alt="" />
-                        </div>
-                        <div className="p-2">
-                            <h3 className="m-0">{pizzaData.name}</h3>
-                            {/* <p>{pizzaData.description}</p> */}
-                        </div>
-                        <div className="cardEnd d-flex justify-content-end pe-3 pb-2    ">
-                            <p className="p-2 m-0">à partir de <span>€{pizzaData.price}</span> </p>
-                            <div className="d-flex align-items-center">
-                                <div className="btnAdd d-flex align-items-center justify-content-center" onClick={() => dispatch(addPizza(pizzaData))} role="button">+</div>
+    export default function Contenu () {
+        // test :
+        const dispatch = useDispatch()
+
+        const [hoveredItem, setHoveredItem] = useState(null);
+
+        return(
+            <>
+            <div className="d-flex content">
+                <div className="contenuA">
+                    {pizzaData.map((pizzaData) => (
+                        <div 
+                            key={pizzaData.name} 
+                            className="cardLink"
+                            onMouseOver={() => setHoveredItem(pizzaData.name)} onMouseOut={() => setHoveredItem(null)}
+                        >
+                            {/* <div className="cardOne"> */}
+                            <div className="cardImg">
+                                <img src={pizzaData.image} alt="" />
                             </div>
+                            <div className={`pizza-texts ${hoveredItem === pizzaData.name ? "focus" : ""}`}>
+                                <div className="p-2">
+                                    <h3 className="m-0">{pizzaData.name}</h3>
+                                    <p className={`pizza-desc ${hoveredItem === pizzaData.name ? "focus" : ""}`}>{pizzaData.description}</p>
+                                </div>
+                            </div>
+                            <div className="cardEnd d-flex justify-content-end pe-3 pb-2">
+                                <p className="p-2 m-0">à partir de <span>€{pizzaData.price}</span></p>
+                                <div className="d-flex align-items-center">
+                                    <div className="btnAdd d-flex align-items-center justify-content-center" onClick={() => dispatch(addPizza(pizzaData))} role="button">+</div>
+                                </div>
+                            </div>
+                            
+                            {/* </div> */}
+
                         </div>
-
-                        {/* </div> */}
-
-                    </div>
-                ))}
-            
+                    ))}
+                
+                </div>
+                <div className="div-cart">
+                    <Cart />
+                </div>
             </div>
-            <div className="div-cart">
-                <Cart />
-            </div>
-        </div>
-        </>
-    )
-}
+            </>
+        )
+    }
